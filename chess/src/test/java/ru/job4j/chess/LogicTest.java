d6a617429d4482cd4585c94511c057ab8d02101a
job4j_elementary <aleksei-korotkov@yandex.ru>
2026-02-12 01:40:05 +0300

    3. Каркас шахматной доски [#792 #558130]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,13 +1,11 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
     @Test
     public void whenMoveThenFigureNotFoundException()
@@ -28,7 +26,8 @@ public class LogicTest {
         ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
             logic.move(Cell.C1, Cell.H5);
         });
-        assertThat(exception.getMessage()).isEqualTo("Could not move by diagonal from %s to %s", Cell.C1, Cell.H5);
+        assertThat(exception.getMessage())
+                .isEqualTo("Could not move by diagonal from %s to %s", Cell.C1, Cell.H5);
     }
 
     @Test
